2f29c3928c963b3f368d9acc181e1c309e1c540f
Dmitro Korostelyov <m0501543116@gmail.com>
2024-08-08 09:33:08 +0300

    New2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def print_hi(name):
-   print(f'Привіт, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+   print(f'Привіт мій друг, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 print_hi('PyCharm')
 
